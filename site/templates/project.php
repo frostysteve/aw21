0670bd9fbb25e95172a7edf37645de6d12ac0b1d
frostysteve <fraserstanley@gmail.com>
2022-02-22 21:15:58 +1100

    reduce project width

--- site/templates/project.php
+++ site/templates/project.php
@@ -3,12 +3,12 @@
 <div class="project">
   <section class="grid grid-cols-12 gap-4 project-content">
 
-    <div class="col-span-10 sm:col-span-5 mt-24 mb-12 font-folio text-4xl">
+    <div class="col-span-10 mt-24 mb-12 font-folio text-4xl">
           <p><?= $page->title() ?></p>
     </div>
 
 
-  <main class="col-span-12 sm:col-span-8 font-epicene mb-40 prose prose-base sm:prose-lg max-w-none">
+  <main class="col-span-12 sm:col-span-6 font-epicene mb-40 prose prose-base sm:prose-lg max-w-none">
     <?php foreach ($page->blocks()->toBlocks() as $block): ?>
     <div id="<?= $block->id() ?>" class="mb-24">
       <?= $block ?>
